9c9a06203cedb213c0699b68a03e7d9d087cc265
zWilliamTM <skilltrail@gmail.com>
2021-03-25 15:10:44 -0500

    First

---
+++ src/components.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: white;
+`;
+
+export const Text = styled.Text`
+  font-size: 18px;
+`;
---
+++ src/root.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import {Container, Text} from './components'
+
+
+export const Root = () => (
+  <Container>
+    <Text>Hello World</Text>
+  </Container>
+)
